98caffa63ac498479bff7b9f8b01ca8f21e0005a
deniskozlov <denis.kozlov94@icloud.com>
2021-06-15 16:29:50 +0300

    some fixes

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ docker build --no-cache -t go_binance_bot .
 docker-compose -f redis/docker-compose.yaml up -d
 sleep 15
 
-docker run --name bot -e BINANCE_API=${API_KEY} -e BINANCE_SECRET=${API_SECRET} -e REDIS_PASSWORD=${REDIS_PASSWORD} -e TICKER=${TICKER} -e TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN} -e TELEGRAM_CHANNEL_NAME={TELEGRAM_CHANNEL_NAME} --network=host -d go_binance_bot
+docker run --name bot -e BINANCE_API=${BINANCE_API} -e BINANCE_SECRET=${BINANCE_SECRET} -e REDIS_PASSWORD=${REDIS_PASSWORD} -e TICKER=${TICKER} -e TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN} -e TELEGRAM_CHANNEL_NAME={TELEGRAM_CHANNEL_NAME} --network=host -d go_binance_bot
